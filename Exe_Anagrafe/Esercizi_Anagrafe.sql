de60f4d5878b611b96f01bade55b15a3c8cfc67a
Firada <lori.scillons@gmail.com>
2022-05-02 16:16:02 +0200

    Aggiunto Exe_Anagrafe_n2°

--- Exe_Anagrafe/Esercizi_Anagrafe.sql
+++ Exe_Anagrafe/Esercizi_Anagrafe.sql
@@ -2,4 +2,11 @@
 
 select p.Nome, c.Regione
 from Persona p join Città c on p.CittàNascita = c.nome
-where p.Età > 18 
+where p.Età >= 18 
+
+
+/*2. Coppie genitori figlio nate nella stessa città*/
+select p1.Nome, p2.Nome
+from Persona p1 join Genia g on p1.Nome = g.Figlio
+    join Persona p2 on p2.Nome = g.Genitore
+where p1.CittàNascita = p2.CittàNascita
