9173df668b2c0cb5c1c71c1774b0b7231e8074e8
Firada <lori.scillons@gmail.com>
2022-05-02 16:16:54 +0200

    Aggiunto Exe_Anagrafe_n3°

--- Exe_Anagrafe/Esercizi_Anagrafe.sql
+++ Exe_Anagrafe/Esercizi_Anagrafe.sql
@@ -10,3 +10,9 @@ select p1.Nome, p2.Nome
 from Persona p1 join Genia g on p1.Nome = g.Figlio
     join Persona p2 on p2.Nome = g.Genitore
 where p1.CittàNascita = p2.CittàNascita
+
+/*3. Città in cui è nato almeno un genitore oltre i 50 anni*/
+
+select distinct p.CittàNascita
+from Persona p join Genia g on p.Nome = g.genitore 
+where p.Età > 50 
