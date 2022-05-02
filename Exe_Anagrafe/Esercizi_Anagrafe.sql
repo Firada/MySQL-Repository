7c3da9050c19b9b24e210f0691e9017a2db445cb
Firada <lori.scillons@gmail.com>
2022-05-02 16:57:47 +0200

    Agg. Alter Join_Implicito Exe_Anagrafe_n2°

--- Exe_Anagrafe/Esercizi_Anagrafe.sql
+++ Exe_Anagrafe/Esercizi_Anagrafe.sql
@@ -11,6 +11,11 @@ from Persona p1 join Genia g on p1.Nome = g.Figlio
     join Persona p2 on p2.Nome = g.Genitore
 where p1.CittàNascita = p2.CittàNascita
 
+/*alternativa Join implicito Exe_n2°
+/* select p1.Nome
+from Persona p, Persona p1, Genia g
+where p.CittàNascita = p1.CittàNascita and ((p.Nome = g.Figlio) and (p1.Nome = g.Genitore)) */
+
 /*3. Città in cui è nato almeno un genitore oltre i 50 anni*/
 
 select distinct p.CittàNascita
