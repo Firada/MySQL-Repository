f6bfe0413b405ab4ddc11a1541c7c14e6a0a4ada
Firada <lori.scillons@gmail.com>
2022-05-04 09:58:18 +0200

    Fix Exe_Programmator_n6°

--- Exe_Programmatori/Exe_Programmatore.sql
+++ Exe_Programmatori/Exe_Programmatore.sql
@@ -3,6 +3,7 @@
 /*1. Calcolare il codice dei programmatori che sono stati autori  di almeno un programma scritto in Java dopo il 2000 */
 
 select a.codice
+/*Non è necessario il Programmatore, sarebbe sufficiente un Join tra Autore e programma, siccome Autore ha "codice" */
 from Autore a join Programmatore p on a.codice = p.codice
     join Programma pr on pr.id = a.id
 where pr.anno > 2000 and pr.linguaggio ='Java'
@@ -20,10 +21,11 @@ order by p.nome
 
 /*3. Per ogni programmatore di categoria 10, calcolare il suo codice e l'anno in cui ha scritto il primo programma in un linguaggio diverso da Java. */
 
-select p.codice, pr.anno
+select p.codice, min(pr.anno), pr.linguaggio
 from Programmatore p join Autore a on p.codice = a.codice
     join Programma pr on pr.id = a.id 
 where p.categoria = 10 and pr.linguaggio != "Java"
+group by p.codice
 
 
 /*4. Calcolare le coppie dei codici di programmatori che sono stati coautori di almeno un programma scritto in Python. */
@@ -46,10 +48,9 @@ group by pr.linguaggio
 /*6. Per ogni programmatore e per ogni anno calcolare il numero di programmi scritti da quel programmatore in quell’anno, mostrando codice del programmatore,
 anno e numero di programmi scritti. */
 
-select  p.codice, pr.anno, count(*)
-from Programmatore p join Autore a on p.codice = a.codice
-    join Programma pr on pr.id = a.id
-group by p.codice, pr.anno
+select  a.codice, pr.anno, count(*)
+from Autore a join Programma pr on a.id = pr.id
+group by a.codice, pr.anno
 
 
  /*7 Per ogni linguaggio calcolare quanti sono in media gli autori dei programmi scritti in quel linguaggio. */
