176dcfbf1071820bfb2ef8cc2527bd953ad8ac97
Firada <lori.scillons@gmail.com>
2022-05-05 11:08:05 +0200

    Fix Exe_Scalatori_n8°

--- Exe_Scalatori/Exe_Scalatori.sql
+++ Exe_Scalatori/Exe_Scalatori.sql
@@ -54,7 +54,7 @@ order by s.anno, n.continente
 nazioni diverse da N. */
 
 
-select s.nazione, count(*)/count(s.anno) as Media
+select s.nazione, count(*)/count( distinct s.anno) as Media
 from Scalatore sc join Scalata s on sc.cf = s.scalatore
 where sc.nazioneNascita != s.nazione
 group by s.nazione
